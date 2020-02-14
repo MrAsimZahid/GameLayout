a8c22a6453cfca509927bfd29353848ebec894e4
Asim Zahid <asimzahid02@gmail.com>
2020-02-14 13:04:03 +0500

    more components added in second activity. Ratings, buttons, etc.

--- app/src/main/java/com/example/gamelayout/MainActivity.java
+++ app/src/main/java/com/example/gamelayout/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    //    setContentView(R.layout.game_full_screen);
         setContentView(R.layout.activity_main);
+
     }
 }
